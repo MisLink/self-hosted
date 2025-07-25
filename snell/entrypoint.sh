18c86fb551d91d665465c088ab3857b107ac45f6
MisLink <gjq.uoiai@outlook.com>
2025-07-26 03:25:24 +0800

    Update
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- snell/entrypoint.sh
+++ snell/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst -no-unset -no-empty -i ./snell-server.conf -o ./snell-server.conf
+envsubst < ./snell-server.conf.tmpl > ./snell-server.conf
 
 exec snell-server -c ./snell-server.conf
